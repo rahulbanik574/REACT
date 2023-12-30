5f59ec36f03fcea1720b3ceafa29da0fa1d41154
rahulbanik574 <rahulbanik574@gmial.com>
2023-12-30 11:02:17 +0530

    Updated

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,10 +1,19 @@
-import React from 'react'//optional line
+"use client";
+import React, { useState } from 'react'//optional line
+
 
 const page = () => {
+  //let marks =80;-react mey humlog esa var nehi banate..we use react hook for making var
+  const [marks, setmarks] = useState(99)
+ 
   return (
-    <div>
-      hello
-    </div>
+    //<></> called fragment ....rapper ka kam korta hey
+   <>
+<h1 className='font-bold'>My mark is {marks}</h1>
+<button  onClick={()=>{
+  setmarks(90)
+}} className='bg-gray-400 py-6 px-6 rounded mt-5 text-white font-bold'>Update</button>
+   </>
   )
 }
 
