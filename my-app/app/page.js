74b40d75d21a7884061bf0380e768efab699f202
rahulbanik574 <rahulbanik574@gmial.com>
2023-12-31 20:53:04 +0530

    Changes

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,22 +1,43 @@
-"use client";
-import React, { useState } from 'react'//optional line
+
+
+// // "use client";
+// // import React, { useState } from 'react'//optional line
 
 
 
-//MAKING A FUNCTION ONCLICK
-const page = () => {
-  //let marks =80;-react mey humlog esa var nehi banate..we use react hook for making var
-  const [marks, setmarks] = useState(99)
+// // //MAKING A FUNCTION ONCLICK
+// // //UseState Hook (variable of react) and real dom and virtual dom
+// // //calling function in react
+// // const page = () => {
+// //   //let marks =80;-react mey humlog esa var nehi banate..we use react hook for making var
+// //   const [marks, setmarks] = useState(99)
  
-  return (
-    //<></> called fragment ....rapper ka kam korta hey
-   <>
-<h1 className='font-bold'>My mark is {marks}</h1>
-<button  onClick={()=>{
-  setmarks(90)
-}} className='bg-gray-400 py-6 px-6 rounded mt-5 text-white font-bold'>Update</button>
-   </>
-  )
-}
+// //   return (
+// //     //<></> called fragment ....rapper ka kam korta hey
+// //    <>
+// // <h1 className='font-bold'>My mark is {marks}</h1>
+// // <button  onClick={()=>{
+// //   setmarks(90)
+// // }} className='bg-gray-400 py-6 px-6 rounded mt-5 text-white font-bold'>Update</button>
+// //    </>
+// //   )
+// // }
 
-export default page
+// // export default page
+
+
+// //BREAKING THE COMPONENT
+// import React from 'react'
+// import Header from "@/Components/Header"
+
+// const page = () => {
+//   return (
+//     <>
+
+// <Header/>
+
+//     </>
+//   )
+// }
+
+// export default page
